45339482e9134249f11793c9a1f6f36b032ef338
Vani <vanishreehv@hotmail.com>
2020-04-28 22:15:41 -0400

    DB Created

--- project1/import1.py
+++ project1/import1.py
@@ -9,14 +9,24 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
+
 def main():
-    f = open("flights.csv")
+    f = open("books.csv")
     reader = csv.reader(f)
-    for origin, destination, duration in reader:
-        flight = Flight(origin=origin, destination=destination, duration=duration)
-        db.session.add(flight)
-        print(f"Added flight from {origin} to {destination} lasting {duration} minutes.")
+
+    for isbn, title, author_fullname, year in reader:
+        try:
+            book = Book(isbn=isbn, title=title, author_fullname=author_fullname,
+                        year=year)
+            db.session.add(book)
+            print(
+                f"Added book isbn =  {isbn} title = {title} author = {author_fullname} published year = {year}")
+        except Exception as e:
+            print(
+                f"Not adding book isbn =  {isbn} title = {title} author = {author_fullname} published year = {year}")
+            continue
     db.session.commit()
+
 
 if __name__ == "__main__":
     with app.app_context():
--- project1/models.py
+++ project1/models.py
@@ -2,16 +2,35 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Flight(db.Model):
-    __tablename__ = "flights"
+
+class Book(db.Model):
+    __tablename__ = "books"
     id = db.Column(db.Integer, primary_key=True)
-    origin = db.Column(db.String, nullable=False)
-    destination = db.Column(db.String, nullable=False)
-    duration = db.Column(db.Integer, nullable=False)
+    isbn = db.Column(db.String, nullable=False, unique=True)
+    title = db.Column(db.String, nullable=False)
+    author_fullname = db.Column(db.String, nullable=False)
+    author_firstname = db.Column(db.String, nullable=True)
+    author_middlename = db.Column(db.String, nullable=True)
+    author_lastname = db.Column(db.String, nullable=True)
+    author_nametitle = db.Column(db.String, nullable=True)
+    year = db.Column(db.Integer, nullable=False)
 
 
-class Passenger(db.Model):
-    __tablename__ = "passengers"
+class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    flight_id = db.Column(db.Integer, db.ForeignKey("flights.id"), nullable=False)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    userid = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+
+class Reviews(db.Model):
+    __tablename__ = "reviews"
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey(
+        "books.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        "users.id"), nullable=False)
+    review_title = db.Column(db.String, nullable=False)
+    review = db.Column(db.String, nullable=False)
